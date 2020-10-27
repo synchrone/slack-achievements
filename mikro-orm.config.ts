b77b5e13d5ec5e99d297c76e27ecbb5f9e2300e7
Aleksandr Bogdanov <syn@syn.im>
2020-10-27 22:02:06 +0100

    debug for local env

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -4,6 +4,7 @@ import {Reaction} from "./features/count_reactions";
 const config: Options = {
     type: 'sqlite', // one of `mongo` | `mysql` | `mariadb` | `postgresql` | `sqlite`
     dbName: 'reactions.sqlite',
+    debug: process.env.NODE_ENV !== 'production',
     entities: [Reaction],
 };
 
